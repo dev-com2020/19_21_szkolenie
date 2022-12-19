7dc41b5545ece7fb0592967e3c68538f8882c159
devcom2020 <kwp@tlen.pl>
2022-12-19 12:21:57 +0100

    poniedziałek 12:21

---
+++ test.py
@@ -0,0 +1,15 @@
+print("Witaj Pythonie!")
+
+print('Typy danych w Pythonie:')
+print('-' * 20)
+
+ROK = 2022
+print(type(ROK))
+temperatura_powietrza = 2.5
+temperaturaPowietrza = -10.44
+print(type(temperatura_powietrza))
+print((100 + 40) - 12 * 3 / 2)
+print(100 + 40 - (12 * 3) // 3.2)
+print(10 ** 3)
+print(12 % 5)
+print("Różnica temperatur:", temperaturaPowietrza - temperatura_powietrza)
