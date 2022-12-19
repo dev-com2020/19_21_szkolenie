ac1118452435572d818645494008c525f4c6cfd5
devcom2020 <kwp@tlen.pl>
2022-12-19 15:46:54 +0100

    poniedziałek 15:46

--- slownik.py
+++ slownik.py
@@ -7,9 +7,16 @@ oceny = {
               'java': [5, 4, 3, 2]}
 }
 
-print(oceny)
+
 print(oceny['pawel'])
 print(oceny['pawel'][:3])
 print(oceny['tomek'])
 print(oceny['tomek']['java'])
 print(oceny['tomek']['java'][:2])
+print(oceny.items())
+print(oceny.keys())
+print(oceny.values())
+print('maciek' in oceny.keys())
+oceny['darek'] = 3
+oceny['tomek'] = (3,)
+print(oceny)
