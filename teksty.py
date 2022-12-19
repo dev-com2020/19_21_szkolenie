c9eebaf1f1646fb74ca5c5c68f3ed8c1379814ae
devcom2020 <kwp@tlen.pl>
2022-12-19 13:09:07 +0100

    poniedziałek 13:09

--- teksty.py
+++ teksty.py
@@ -1,1 +1,29 @@
+'''
+komentarz
+komentarz
+komentarz
+komentarz
+'''
+
 imie = "Tomek"
+wiek = "39"
+print(type(imie))
+print(type(wiek))
+print("Wynik mnożenia:", wiek * 2)  # tutaj jest błąd!
+print("Wynik łączenia:", imie + wiek)
+print(imie.upper())
+print(imie.lower())
+print(imie.title())
+print(imie.count("Tom"))
+print(imie.join('----'))
+print(imie[0:3])
+print(imie[:3])
+print(imie[-3:-1])
+print(imie[-2:])
+
+
+
+
+
+
+
