405835b7eead22de0fa52c6a9bf61a6934211007
devcom2020 <kwp@tlen.pl>
2022-12-20 12:45:35 +0100

    wtorek 12:45

--- funkcje.py
+++ funkcje.py
@@ -6,9 +6,14 @@ def drukarka(tekst, ile_razy):
     wydruk = (tekst + " ") * ile_razy
     return wydruk
 
+
 def drukarka2(tekst, ile_razy):
     return (tekst + " ") * ile_razy
 
 
-lista = [drukarka("NAPIS", 5), drukarka2("NAPIS", 2), drukarka("NAPIS", 1), "tomek"]
+def drukarka3(tekst, ile_razy=1):
+    return (tekst + " ") * ile_razy
+
+
+lista = [drukarka("NAPIS", 5), drukarka2("NAPIS", 2), drukarka3(ile_razy=5, tekst="5"), "tomek"]
 print(lista)
