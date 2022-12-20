a16fb870f92a4a0b632fd9a9f277c7de144cc6ac
devcom2020 <kwp@tlen.pl>
2022-12-20 11:20:05 +0100

    wtorek 11:20

---
+++ petle.py
@@ -0,0 +1,29 @@
+lista = [4, 6, 8, 0]
+wyniki = []
+
+for liczba in lista:
+    wyniki.append(liczba * 2)
+
+print(wyniki)
+
+for i in range(100, 200, 10):
+    print(i)
+
+liczby = list(range(1, 11, 2))
+print(liczby)
+
+imiona = ['Tomek', 'Bartek', 'Ania', 'Paweł']
+lata = [23, 22, 21]
+# for p in range(len(imiona)):
+#     print(p, imiona[p])
+
+# for poz, imie in enumerate(imiona):
+#     wiek = lata[poz]
+#     print(imie, wiek)
+
+# for person, age in zip(imiona, lata):
+#     print(person, age)
+
+#
+# for poz, imie in enumerate(imiona, 1):
+#     print(imie, poz)
