3091ada7aedaf4949c62cee847dd3240cc460e2f
devcom2020 <kwp@tlen.pl>
2022-12-20 10:10:44 +0100

    wtorek 10:10

--- petle.py
+++ petle.py
@@ -7,13 +7,28 @@
 #
 # print("...Kolejne instrukcje programu...")
 
-znaPythona = input("Czy znasz język?(Wprowadź 't' lub 'n')")
+# znaPythona = input("Czy znasz język?(Wprowadź 't' lub 'n')")
+#
+# if znaPythona == 'tak' or znaPythona == 'Tak' or znaPythona == 't':
+#     print("Gratulacje!")
+# elif znaPythona == 'nie':
+#     print("Musisz się nauczyć!")
+# else:
+#     print("Nie ma takiego wyboru!")
+#
+# print("...Kolejne instrukcje programu...")
 
-if znaPythona == 'tak' or znaPythona == 'Tak' or znaPythona == 't':
-    print("Gratulacje!")
-elif znaPythona == 'nie':
-    print("Musisz się nauczyć!")
-else:
-    print("Nie ma takiego wyboru!")
+# podatek = 9999
+# if 500 <= podatek <= 1000:
+#     oplata = 0.0
+# elif podatek < 3000:
+#     oplata = 0.2
+# elif podatek < 5000:
+#     oplata = 0.35
+# elif podatek != 9999:
+#     oplata = -0.2
+# else:
+#     oplata = 0.45
+#
+# print(oplata)
 
-print("...Kolejne instrukcje programu...")
