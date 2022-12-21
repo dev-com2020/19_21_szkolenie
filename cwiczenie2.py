3a733b8db6afed59dafa07209a0e1ad8aec9cb10
devcom2020 <kwp@tlen.pl>
2022-12-21 10:42:33 +0100

    sroda 10:42

---
+++ cwiczenie2.py
@@ -0,0 +1,13 @@
+import json
+
+# with open('dane/hero.json', 'r') as f:
+#     data = json.load(f)
+#
+# print(data)
+# print(data["members"])
+# print(data["members"][0])
+# lista = data["members"][0]['powers']
+# moc = data["members"][0]['powers'][-1]
+# print(f"Ostatnia moc bohatera to {moc}")
+# for i in lista:
+#     print(i)
