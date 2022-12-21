ea5831a4a4e4133f1ba7a9b8ac01eb654d95d6eb
devcom2020 <kwp@tlen.pl>
2022-12-21 08:11:17 +0100

    sroda 08:11

---
+++ start.py
@@ -0,0 +1,10 @@
+# from program.matematyka import dodaj, odejmij, pomnoz
+from program import *
+
+
+print("Witaj w moim programie!")
+
+print(matematyka.dodaj(5, 5))
+print(matematyka.odejmij(10, 5))
+print(matematyka.pomnoz(4, 2))
+print(podziel(4, 2))
